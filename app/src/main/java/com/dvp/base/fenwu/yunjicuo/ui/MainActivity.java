c8427896696f19511c70b5e420d7c69730144847
Administrator <dfwmaomao@163.com>
2016-06-15 17:35:57 +0800

    检测新版本添加

--- app/src/main/java/com/dvp/base/fenwu/yunjicuo/ui/MainActivity.java
+++ app/src/main/java/com/dvp/base/fenwu/yunjicuo/ui/MainActivity.java
@@ -68,8 +68,6 @@ public class MainActivity extends CommonActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
 
 
